d0a3446b75bcb01661fcc64a941a3f6c46027194
kienph <phihuukien08072002@gmail.com>
2022-12-27 21:55:21 +0700

    Add project files.

---
+++ Doctors_Web_Forum_FE/Areas/admin/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doctors_Web_Forum_FE.Areas.admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    public class HomeController : Controller
+    {
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Doctors_Web_Forum_FE/Controllers/QuestionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doctors_Web_Forum_FE.Controllers
+{
+    [Route("question")]
+    public class QuestionController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View("Question");
+        }
+    }
+}
