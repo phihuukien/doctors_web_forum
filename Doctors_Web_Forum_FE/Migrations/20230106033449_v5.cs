c42ea313137e6c092a8086efe97bc6f2c928a4a3
kienph <phihuukien08072002@gmail.com>
2023-01-11 20:17:32 +0700

    update project

---
+++ Doctors_Web_Forum_FE/Migrations/20230106033449_v5.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Doctors_Web_Forum_FE.Migrations
+{
+    public partial class v5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CodeToken",
+                table: "Account",
+                newName: "Token");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Token",
+                table: "Account",
+                newName: "CodeToken");
+        }
+    }
+}
---
+++ Doctors_Web_Forum_FE/Migrations/20230111124542_v6.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Doctors_Web_Forum_FE.Migrations
+{
+    public partial class v6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Account");
+
+            migrationBuilder.RenameColumn(
+                name: "Work_Place",
+                table: "Account",
+                newName: "Location");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Location",
+                table: "Account",
+                newName: "Work_Place");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Position",
+                table: "Account",
+                maxLength: 200,
+                nullable: true);
+        }
+    }
+}
